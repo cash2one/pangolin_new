78add797b2f97100853f8ead2d1ebfcfdc700e7b
skylife <skylife@ukr.net>
2016-12-02 23:22:45 +0200

    notesapp

--- static/pangolin/js/notes.js
+++ static/pangolin/js/notes.js
@@ -13,23 +13,35 @@ var Note = React.createClass({
 var NoteEditor = React.createClass({
     getInitialState: function() {
         return {
-            text: ''
+            text: '',
+            isButtonActive: false
         };
     },
 
     handleTextChange: function(event) {
+      const text = event.target.value;
+
+      if (text.length === 0) {
+        this.setState({ text: '' });
+      } else {
         this.setState({ text: event.target.value });
+        this.setState({ isButtonActive: true });
+      }
+
     },
 
     handleNoteAdd: function() {
+
         var newNote = {
             text: this.state.text,
             color: 'yellow',
             id: Date.now()
         };
-
+        if (this.state.isButtonActive) {
         this.props.onNoteAdd(newNote);
         this.setState({ text: '' });
+        this.setState({ isButtonActive: false });
+      }
     },
 
     render: function() {
@@ -37,12 +49,16 @@ var NoteEditor = React.createClass({
             <div className="note-editor">
                 <textarea
                     placeholder="Enter your note here..."
-                    rows={5}
+                    rows={4}
                     className="textarea"
                     value={this.state.text}
                     onChange={this.handleTextChange}
                 />
+                { (!this.state.isButtonActive) ? (
+                <button className="not-button">Type</button>
+              ) : (
                 <button className="add-button" onClick={this.handleNoteAdd}>Add</button>
+              )}
             </div>
         );
     }
